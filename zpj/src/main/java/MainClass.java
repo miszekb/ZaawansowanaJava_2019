2d2d83c720e68d5cfc670cdb9645cad7976c3ffb
Michał Białecki <miszekb@gmail.com>
2019-03-18 19:03:49 +0100

    first build to test Travis CI

---
+++ zpj/src/main/java/MainClass.java
@@ -0,0 +1,11 @@
+
+public class MainClass {
+
+    //tiny example to test Travis continuous integration
+    public static void main(String[] args)
+    {
+        System.out.print("halooo");
+    }
+
+
+}
