cef04100b81d485ed405422ae7ab4182b3e99d48
Marcin <210211@edu.p.lodz.pl>
2019-04-28 21:16:16 +0200

    Api for exchanging currencies
    Dodałem obsługę api z walutami. Pobieram api z NBP w formacie json i obsługuję je w kodzie. Dostępne opcje to GetCurrencyRate - zwraca wartość podanej waluty (z notowań z obecnego dnia) i GetCurrencyRateOnDate - zwraca wartość podanej waluty w podanym dniu. Przykład w main(). Nie zrobiłem jeszcze testów, ale wkrótce je zrobie. PS: Dodałem do pliku pom dependency.

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,3 +1,7 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.Date;
 
 public class MainClass {
@@ -5,7 +9,7 @@ public class MainClass {
     //tiny example to test Travis continuous integration
 
 
-    public static void main(String[] args)
+    public static void main(String[] args) throws IOException, JSONException
     {
         Date date = new Date(2019,3,12);
         PastPayment pastPayment = new PastPayment(1,"pszne obiad",21.37f,Categories.Transport, "bardzo pszne", date);
@@ -14,6 +18,12 @@ public class MainClass {
         DrawingClass drawingClass = new DrawingClass(pastPaymentRepository.getRepo());
         drawingClass.DebugPaymentsInMonth(3);
         //System.out.print(pastPayment.toString());
+
+
+        float rate = CurrencyExchanger.GetCurrencyRate("USD");
+        float rate2 = CurrencyExchanger.GetCurrencyRateOnDate("USD", "2016-01-15");
+        System.out.println(rate);
+        System.out.println(rate2);
     }
 
 
